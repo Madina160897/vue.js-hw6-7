4414bc9591f17bcfd346f217d2a1d5c09e22dab7
Жалимбетова Мадина Азаматкызы <ZhalimbetovaM@lcoast.itstep>
2023-02-09 21:35:19 +0600

    end

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -1,8 +1,147 @@
 <script>
+export default {
+  data() {
+    return {
+      contacts: [
+        {
+          name: "Madina",
+          number: 87000000000,
+        },
+        {
+          name: "Elya",
+          number: 87000000001,
+        },
+        {
+          name: "Anara",
+          number: 87000000002,
+        },
+        {
+          name: "Aslan",
+          number: 87000000003,
+        },
+        {
+          name: "Danik",
+          number: 87000000004,
+        },
+        {
+          name: "Gleb",
+          number: 87000000005,
+        },
+        {
+          name: "Iliyas",
+          number: 87000000006,
+        }
+      ],
+      name: "",
+      number: "",
+    };
+  },
+  methods: {
+    add() {
+      if (this.name === "" || this.number === "") {
+        return;
+      }
+
+      let newContact = {
+        id: this.contacts.length + 1,
+        name: this.name,
+        number: this.number,
+      };
+
+      this.contacts.push(newContact);
+      this.name = "";
+      this.number = "";
+    },
+    deleteCon(id) {
+      for (let i = 0; i < this.contacts.length; i++) {
+        if (this.contacts[i].id == id) {
+          this.contacts.splice(i, 1);
+        }
+      }
+    },
+  },
+};
 </script>
 
 <template>
+  <div>
+    <header>
+      <div class="header-icon">
+        <router-link to="/mainPage">
+          <button class="btn">Главная страница</button>
+        </router-link>
+        <router-link to="/gallery">
+          <button class="btn">Галерея</button>
+        </router-link>
+      </div>
+    </header>
+    <div>
+      <form class="Post" @submit.prevent>
+        <input v-model="name" type="text" placeholder="Имя пользователя" />
+        <input
+          v-model="number"
+          type="number"
+          placeholder="Номер пользователя"
+        />
+        <button class="btn" @click="add">Добавить</button>
+      </form>
+      <br />
+      <div class="fon-cont">
+        <div class="contact" v-for="con in contacts" :key="con.id">
+          <b>{{ con.name }}:</b>
+          <div>{{ con.number }}</div>
+          <button class="btn-con" @click="deleteCon(con.id)">X</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
+.Post {
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  margin: 1% auto;
+  border: 2px solid #2dcdd3;
+  background-color: #f4f7f7;
+  border-radius: 20px;
+  padding: 5%;
+  font-size: 16px;
+  margin-top: 50px;
+}
+
+input {
+  margin-bottom: 20px;
+  padding: 10px;
+  font-size: 16px;
+  border: 2px solid #5c9ea1;
+  background-color: #ffffff9c;
+  border-radius: 15px;
+}
+
+.contact {
+  display: flex;
+  justify-content: space-around;
+  border: 2px solid #2dcdd3;
+  background-color: #f4f7f7;
+  border-radius: 20px;
+  padding: 2%;
+  font-size: 16px;
+  width: 20%;
+  margin-top: 10px;
+}
+
+.fon-cont {
+  width: 80%;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  flex-wrap: wrap;
+}
+
+.btn-con {
+  background-color: transparent;
+  border: 1px solid transparent;
+}
 </style>
--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,114 +1,153 @@
 <script>
 export default {
-    data() {
-        return {
-            images: [
-                {
-                    id: 1,
-                    image: "/src/img/img1.jpg"
-                },
-                {
-                    id: 2,
-                    image: "/src/img/img2.jpg"
-                },
-                {
-                    id: 3,
-                    image: "/src/img/img3.jpg"
-                },
-                {
-                    id: 4,
-                    image: "/src/img/img4.jpg"
-                },
-                {
-                    id: 5,
-                    image: "/src/img/img5.jpg"
-                },
-            ]
-        }
-    }
-}
+  data() {
+    return {
+      images: [
+        {
+          id: 1,
+          image: "/src/img/img1.jpg",
+        },
+        {
+          id: 2,
+          image: "/src/img/img2.jpg",
+        },
+        {
+          id: 3,
+          image: "/src/img/img3.jpg",
+        },
+        {
+          id: 4,
+          image: "/src/img/img4.jpg",
+        },
+        {
+          id: 5,
+          image: "/src/img/img5.jpg",
+        },
+      ],
+      numImg: 0,
+    };
+  },
+  methods: {
+    next() {
+      if (this.numImg >= this.images.length - 1) {
+        this.numImg = 0;
+      }
+      this.numImg++;
+    },
+    pev() {
+      if (this.numImg >= 0) {
+        this.numImg--;
+      }
+    },
+  },
+};
 </script>
 
 <template>
-    <div>
-        <header>
-            <div class="header-icon">
-                <router-link to="/mainPage">
-                    <button class="btn">Главная страница</button>
-                </router-link>
-                <router-link to="/contacts">
-                    <button class="btn">Контакты</button>
-                </router-link>
-            </div>
-        </header>
-        <main>
-            <div v-for="img in images">
-                <img class="img-gal" :src="img.image">
-            </div>
-        </main>
-        <div class="all-btn-gal">
-            <button> <img class="icon" src="../img/left.png" ></button>
-            <button> <img class="icon" src="../img/right.png" ></button>
+  <div>
+    <header>
+      <div class="header-icon">
+        <router-link to="/mainPage">
+          <button class="btn">Главная страница</button>
+        </router-link>
+        <router-link to="/contacts">
+          <button class="btn">Контакты</button>
+        </router-link>
+      </div>
+    </header>
+    <main>
+      <div class="wrapper">
+        <div class="block" :style="{ 'margin-left': '-' + 100 * numImg + '%' }">
+          <div class="scale" v-for="img in images" :key="img.id">
+            <img class="img-gal" :src="img.image" />
+          </div>
         </div>
+      </div>
+    </main>
+    <div class="all-btn-gal">
+      <button @click="pev"><img class="icon" src="../img/left.png" /></button>
+      <button @click="next"><img class="icon" src="../img/right.png" /></button>
     </div>
+  </div>
 </template>
 
 <style>
+.scale {
+  transition: 1s; /* Время эффекта */
+}
+.scale:hover {
+  transform: scale(1.2); /* Увеличиваем масштаб */
+}
+.wrapper {
+  max-width: 900px;
+  max-height: 600px;
+  overflow: hidden;
+  margin: 0 auto;
+}
+
+.wrapper:hover{
+   max-width: 2200px;
+   max-height: 1900px;
+}
+
+.block {
+  display: flex;
+  transition: all ease 0.5s;
+}
 header {
-    width: 100%;
-    height: 70px;
-    margin: 20px auto;
-    border: 2px solid transparent;
-    box-shadow: 0 0 5px gray;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
+  width: 100%;
+  height: 70px;
+  margin: 20px auto;
+  border: 2px solid transparent;
+  box-shadow: 0 0 5px gray;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 }
 
-.img-gal{
-    width: 900px;
-    height: 600px;
+.img-gal {
+  width: 1900px;
+  height: 1000px;
 }
 
-main{
-    width: 100%;
-    height: 800px;
-    margin: 20px auto;
-    border: 2px solid black;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+main {
+  width: 100%;
+  height: 700px;
+  margin: 20px auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-.header-icon{
-    width: 80%;
-    height: 50px;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
+.header-icon {
+  width: 80%;
+  height: 50px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 }
 
-.btn{
-    width: 250px;
-    height: 50px;
-    border: 2px solid transparent;
-    box-shadow: 0 0 5px gray;
-    border-radius: 20px;
-    background-color: skyblue;
-    font-size: 16px;
-    font-weight: 600;
+.btn {
+  width: 250px;
+  height: 50px;
+  border: 2px solid transparent;
+  box-shadow: 0 0 5px gray;
+  border-radius: 20px;
+  background-color: skyblue;
+  font-size: 16px;
+  font-weight: 600;
 }
 
-.all-btn-gal{
-    width: 400px;
-    margin: 0 auto;
-    display: flex;
-    justify-content: space-between;
+.all-btn-gal {
+  width: 400px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
 }
 
-.icon{
-    width: 50px;
-    height: 50px;
+.icon {
+  width: 50px;
+  height: 50px;
 }
 </style>
